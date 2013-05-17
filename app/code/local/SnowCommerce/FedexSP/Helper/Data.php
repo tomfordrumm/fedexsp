7d12250cce56edefc6bf54a669fec554009438a5
Tomfordrumm <tomfordrumm@gmail.com>
2013-05-17 20:10:19 +0400

    first commit

---
+++ app/code/local/SnowCommerce/FedexSP/Helper/Data.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: svatoslavzilicev
+ * Date: 14.05.13
+ * Time: 16:20
+ * To change this template use File | Settings | File Templates.
+ */ 
+class SnowCommerce_FedexSP_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
---
+++ app/code/local/SnowCommerce/FedexSP/controllers/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: svatoslavzilicev
+ * Date: 16.05.13
+ * Time: 1:00
+ * To change this template use File | Settings | File Templates.
+ */
+class SnowCommerce_FedexSP_IndexController extends Mage_Core_Controller_Front_Action{
+
+    public function indexAction(){
+        die('motherfucker!!');
+    }
+}
